471c75a363cb262203d11e974fc5d33ca4e0d5bd
indiprosolutionsweb-lab <indiprosolutionsweb@gmail.com>
2025-09-03 11:33:14 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,3 +4,7 @@ import { Day } from './types';
 export const DAYS: Day[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 export const TOTAL_WEEKS = 52;
+
+// Feature flag to control visibility of time tracking.
+// Set to `true` to show time inputs and analysis, `false` to hide.
+export const isTimeTrackingEnabled = false;
