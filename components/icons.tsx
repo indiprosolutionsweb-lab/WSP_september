102fb3d5905f6ddda5f112e92705fd60ae45d09b
indiprosolutionsweb-lab <indiprosolutionsweb@gmail.com>
2025-09-24 17:04:49 +0530

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -48,3 +48,9 @@ export const CollapseIcon = () => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4H4v4m12-4h4v4m-4 12h4v-4m-12 4H4v-4" />
     </svg>
 );
+
+export const CalendarIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+    </svg>
+);
